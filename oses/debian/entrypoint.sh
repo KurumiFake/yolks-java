0b154d38223e6491624cba83ef8cb577c2a3fcfd
KurumiFake <kurumiisshidowife@gmail.com>
2022-04-29 14:14:21 +0700

    how bloated is it? y e s

--- oses/debian/entrypoint.sh
+++ oses/debian/entrypoint.sh
@@ -26,7 +26,7 @@ TZ=${TZ:-UTC}
 export TZ
 
 # Set environment variable that holds the Internal Docker IP
-INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
+INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-0);exit}')
 export INTERNAL_IP
 
 # Switch to the container's working directory
@@ -39,6 +39,9 @@ PARSED=$(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g' | eval echo "$(cat
 
 # Display the command we're running in the output, and then execute it with the env
 # from the container itself.
-printf "\033[1m\033[33mcontainer@pterodactyl~ \033[0m%s\n" "$PARSED"
+
+#nah no
+#printf "\033[1m\033[33mcontainer@pterodactyl~ \033[0m%s\n" "$PARSED"
+
 # shellcheck disable=SC2086
 exec env ${PARSED}
